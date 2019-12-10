3a06531b2ddfcb43e8f66c315ef1ac5fb95ac233
github username <github email address>
2019-12-10 14:27:51 -0500

    add controller crud methods

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,32 @@
 class EntriesController < ApplicationController
+  
+  def index
+    @entries = Entry.all
+    render json: @entries
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+       render json: @entry
+     else
+       render json: {error: 'Error creating entry'}
+     end
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+    render json: @entry
+  end
+
+  def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:title, :content, :date)
+  end
 end
